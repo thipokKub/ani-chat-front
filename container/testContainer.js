e19afffac53594defce184341b1c8ce76a023442
thipokKub <thipok30386@gmail.com>
2017-11-16 14:32:00 +0700

    add HOC

--- container/testContainer.js
+++ container/testContainer.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import withStyle from '../hoc/withStyle';
 import autoBind from '../hoc/autoBind';
+import withInternalState from '../hoc/withInternalState';
 import stylesheet from './style/testContainer.scss';
 
-export default withStyle(autoBind((props) => {
+function onAddOne(state) {
+    return state.test + 1;
+}
+
+export default withInternalState(withStyle(autoBind((props) => {
     return (
-        <div className="test-container" onClick={() => props.testMessage("Helloo")}>
-            Store string: {props.test.String}
+        <div
+            className="test-container"
+            onClick={() => {
+                props.testMessage("Helloo");
+                props.onSetState('test', onAddOne(props.state))
+            }}
+        >
+            Store string: {props.test.String}<br />
+            Internal State string: {props.state.test}
         </div>
     )
-}), [stylesheet]);
+}), [stylesheet]), {
+    'test': 1
+});
